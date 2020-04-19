e4179e2a7c46c494ce6236f460575053ac44d543
Jiancao164 <60164697+Jiancao164@users.noreply.github.com>
2020-04-19 15:54:05 -0700

    fix bugs

--- src/components/homePage/HomeHeading.js
+++ src/components/homePage/HomeHeading.js
@@ -100,35 +100,26 @@ class HomeHeading extends Component{
                             <MoviePrototype
                                 history = {this.props.history}
                                 latestTitleSearch={this.props.match.params.latestTitleSearch}/>}
-                            {this.props.match.path === `/` && <div className={"row"}>
-                                <div>
-                                    {this.props.match.path === `/` &&
-                                    <div>
-                                        <div className={"col-8"}>
-                                            {this.props.match.path === `/` &&
-                                            <NewMovies/>
-                                            }
-                                        </div>
-                                        <div className={"col-4"}>
-                                            <br/>
-                                            {!this.state.profile.username}
-                                            {this.state.profile.username && this.props.match.path === `/` &&
-                                            <div>
-                                                <h2 className={"now-playing"}>Movies Reviewed</h2>
-                                                <ReviewedMovie reviews={this.state.reviews}/>
-                                            </div>
-                                            }
-
-                                        </div>
-                                    </div>
-                                    }
-                                </div>
-
-                            </div>}
                         </div>
+                    <div className={"row"}>
+                    <div className={"col-8"}>
+                        {this.props.match.path === `/` &&
+                        <NewMovies/>
+                        }
+                    </div>
+                    <div className={"col-4"}>
+                        <br/>
+                        {!this.state.profile.username}
+                        {this.state.profile.username && this.props.match.path === `/` &&
+                        <div>
+                            <h2 className={"now-playing"}>Movies Reviewed</h2>
+                            <ReviewedMovie reviews={this.state.reviews}/>
+                        </div>
+                        }
 
-                    </div>}
-                }
+                    </div>
+                    </div>
+                </div>}
                 {!this.state.profile.username &&
                 <div className={"container new-movie-width"}>
                     {this.props.match.path === `/` &&
