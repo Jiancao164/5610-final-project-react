78b9a99b10867d73d24d7049c5ca154ab582d6ba
Jian Cao <jiancao164@gmail.com>
2022-04-23 22:04:38 -0700

    update endpoint to ec2

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,2 +1,2 @@
 //export const HEROKU_SERVER_URL = "http://localhost:8080";
-export const HEROKU_SERVER_URL = "http://ec2-35-174-105-5.compute-1.amazonaws.com:5000";
+export const HEROKU_SERVER_URL = "http://ec2-54-234-179-197.compute-1.amazonaws.com:8080";
