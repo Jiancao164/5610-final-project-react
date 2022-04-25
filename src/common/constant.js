6bf7cf4814eb6153d3e79556a2514a79c071ad9d
Jian Cao <jiancao164@gmail.com>
2022-04-24 17:40:17 -0700

    make it better

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,4 +1,4 @@
 // export const HEROKU_SERVER_URL = "http://localhost:8080";
 // export const HEROKU_SERVER_URL = "http://ec2-54-234-179-197.compute-1.amazonaws.com:8080";
 //export const HEROKU_SERVER_URL = "http://app-server-load-balancer-1403416556.us-east-1.elb.amazonaws.com:8080"
-export const HEROKU_SERVER_URL = "https://jian-web-service.herokuapp.com/"
+export const HEROKU_SERVER_URL = "https://jian-web-service.herokuapp.com"
