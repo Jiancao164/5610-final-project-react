efc70248aadd1e277caa418488f2e37d070b13dc
Jian Cao <jiancao164@gmail.com>
2022-04-29 15:34:56 -0700

    update load balancer

--- src/common/constant.js
+++ src/common/constant.js
@@ -3,4 +3,4 @@
 //export const HEROKU_SERVER_URL = "https://app-server-load-balancer-318246495.us-east-1.elb.amazonaws.com:8080"
 //export const HEROKU_SERVER_URL = "http://ex09-821717664.us-east-1.elb.amazonaws.com:8080"
 
-export const HEROKU_SERVER_URL = "https://cors-everywhere.herokuapp.com//http://ex09-821717664.us-east-1.elb.amazonaws.com:8080"
+export const HEROKU_SERVER_URL = "https://cors-everywhere.herokuapp.com/http://ex09-821717664.us-east-1.elb.amazonaws.com:8080"
